e69716743a87fb09daa88a884eba0e96fbd8a123
mehran <hosseinzadehmehran4@gmail.com>
2024-02-01 19:33:31 +0330

    fix(Authentication): move email verification method from rest controller to a new auth controller in web directory

--- app/Http/Controllers/REST/Auth/AuthController.php
+++ app/Http/Controllers/REST/Auth/AuthController.php
@@ -5,26 +5,22 @@ namespace App\Http\Controllers\REST\Auth;
 use App\Exceptions\ServiceCallException;
 use App\Http\Controllers\APIController;
 use App\Http\Requests\CreateUserRequest;
-use App\Services\Contracts\EmailVerificationInterface;
 use App\Services\Contracts\UserRegistrationInterface;
 use Illuminate\Http\JsonResponse;
-use Illuminate\Http\RedirectResponse;
-use Illuminate\Http\Request;
+
 
 class AuthController extends APIController
 {
     private UserRegistrationInterface $userRegistrationService;
-    private EmailVerificationInterface $emailVerificationService;
+
 
     /**
      * @param UserRegistrationInterface $userRegistration
-     * @param EmailVerificationInterface $emailVerificationService
      */
-    public function __construct(UserRegistrationInterface $userRegistration, EmailVerificationInterface $emailVerificationService)
+    public function __construct(UserRegistrationInterface $userRegistration)
     {
         parent::__construct();
         $this->userRegistrationService = $userRegistration;
-        $this->emailVerificationService = $emailVerificationService;
     }
 
     /**
@@ -43,22 +39,5 @@ class AuthController extends APIController
         return $response;
     }
 
-    /**
-     * @param Request $request
-     * @return RedirectResponse
-     * @throws \Exception
-     */
-    public function verifyEmail(Request $request): RedirectResponse
-    {
-        try {
-            if ($this->emailVerificationService->verifyEmail($request->token)) {
-                $redirect_address = config('account.email_verification_success_redirect_address');
-            } else {
-                throw new \Exception('failed to verify email address');
-            }
-        } catch (\Exception $exception) {
-            $redirect_address = config('account.email_verification_failure_redirect_address');
-        }
-        return redirect($redirect_address);
-    }
+
 }
