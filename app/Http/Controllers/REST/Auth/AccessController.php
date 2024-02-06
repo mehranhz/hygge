fd39d3262f3a524ecd6fb74832350e95c74cb582
Mehran <hosseinzadehmehran4@gamil.com>
2024-02-06 14:21:32 +0330

    fix(AccessControl): add phpDocs in role assignment controller

--- app/Http/Controllers/REST/Auth/AccessController.php
+++ app/Http/Controllers/REST/Auth/AccessController.php
@@ -4,21 +4,28 @@ namespace App\Http\Controllers\REST\Auth;
 
 use App\Exceptions\ServiceCallException;
 use App\Http\Controllers\APIController;
-use App\Http\Controllers\Controller;
 use App\Http\Requests\REST\RoleToUserAssignmentRequest;
 use App\Services\Contracts\RoleAssignmentInterface;
-use Illuminate\Http\Request;
+use Illuminate\Http\JsonResponse;
 
 class AccessController extends APIController
 {
     private RoleAssignmentInterface $roleAssignmentService;
 
+
+    /**
+     * @param RoleAssignmentInterface $roleAssignmentService
+     */
     public function __construct(RoleAssignmentInterface $roleAssignmentService)
     {
         parent::__construct();
         $this->roleAssignmentService = $roleAssignmentService;
     }
 
+    /**
+     * @param RoleToUserAssignmentRequest $request
+     * @return JsonResponse
+     */
     public function assignRoleToUser(RoleToUserAssignmentRequest $request)
     {
         try {
